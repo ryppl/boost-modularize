2b9c1899f4e4fa60c58c8ca8dc327f60a0fa6a02
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-05-16 10:19:17 +0200

    boost/version.hpp will be configured by cmake

--- modularize.py
+++ modularize.py
@@ -456,7 +456,7 @@ def push_modules():
 
     # Push this change up to the origin
     print 'Pushing the boost supermodule...'
-    run('git', 'push', 'git@github.com:boost-lib/boost-supermodule.git', 'master', cwd=dst_repo_dir)
+    run('git', 'push', 'git@github.com:boost-lib/boost.git', 'master', cwd=dst_repo_dir)
 
     print 'Done'
 
