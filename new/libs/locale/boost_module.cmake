273f3771cbfe3fa4b3e4dda56d50961e6ebf329f
Daniel Pfeifer <daniel@pfeifer-mail.de>
2011-08-04 12:19:56 +0200

    metadata updated

---
+++ new/libs/locale/boost_module.cmake
@@ -0,0 +1,16 @@
+# Distributed under the Boost Software License, Version 1.0.
+# See http://www.boost.org/LICENSE_1_0.txt
+
+boost_module(Locale
+  DEPENDS
+    config
+    function
+    integer
+    iterator
+    smart_ptr
+    thread
+    unordered
+    utility
+  INCLUDE_DIRECTORIES
+    include
+  )
