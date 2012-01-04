c305238b88489786bbf3410cfdb99148fbebbaaf
Daniel Pfeifer <daniel@pfeifer-mail.de>
2012-01-04 11:32:53 +0100

    Update copy_listfiles.cmake

--- copy_listfiles.cmake
+++ copy_listfiles.cmake
@@ -2,13 +2,15 @@ if(NOT DEFINED BOOST_DIR)
   set(BOOST_DIR "${CMAKE_CURRENT_LIST_DIR}/../boost")
 endif()
 
+get_filename_component(boost ${BOOST_DIR} NAME)
+
 file(COPY "${BOOST_DIR}/"
   DESTINATION "${CMAKE_CURRENT_LIST_DIR}/new"
   FILES_MATCHING
-    PATTERN ".git"                 EXCLUDE
-    PATTERN "boost/build"          EXCLUDE # binary dir
-    PATTERN "boost/tools/cmake"    EXCLUDE # has its own listfiles
-    PATTERN "boost/CMakeLists.txt" EXCLUDE
+    PATTERN ".git"                    EXCLUDE
+    PATTERN "${boost}/build"          EXCLUDE # binary dir
+    PATTERN "${boost}/tools/cmake"    EXCLUDE # has its own listfiles
+    PATTERN "${boost}/CMakeLists.txt" EXCLUDE
     PATTERN "boost_module.cmake"
     PATTERN "CMakeLists.txt"
   )
