cba1ded18c0d94df0b91f6c5f5058f841d4fd149
Daniel Pfeifer <daniel@pfeifer-mail.de>
2012-02-26 10:35:40 +0100

    added context and local_function

---
+++ new/libs/local_function/boost_module.cmake
@@ -0,0 +1,15 @@
+# Distributed under the Boost Software License, Version 1.0.
+# See http://www.boost.org/LICENSE_1_0.txt
+
+boost_module(LocalFunction
+  DEPENDS
+    config
+    mpl
+    preprocessor
+    scope_exit
+    type_traits
+    typeof
+    utility
+  INCLUDE_DIRECTORIES
+    include
+  )
