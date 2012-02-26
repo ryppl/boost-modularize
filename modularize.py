a12a1c4c83f9ada3fb203bb96dd959e3dece9f98
Daniel Pfeifer <daniel@pfeifer-mail.de>
2012-02-26 11:10:44 +0100

    modularize.py fix

--- modularize.py
+++ modularize.py
@@ -133,6 +133,7 @@ class Module:
             run('git', 'config', 'branch.master.remote', 'origin', cwd=self.dst_dir)
             run('git', 'config', 'branch.master.merge', 'refs/heads/master', cwd=self.dst_dir)
             run('git', 'submodule', 'add', repo_ro % base, self.section, cwd=self.dstroot)
+            run('git', 'commit', '-am', 'Added submodule %s' % base, cwd=self.dstroot)
 
     # remove everything (leaving behind the top-level .git directory)
     def clean(self):
